e32853693528f7dbbacb4258b539607420774b71
richadiago <iagorichard@outlook.com>
2017-09-23 23:50:18 -0300

    paridade de numeros inteiros

---
+++ lista01/01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+	int x;
+	scanf("Digite um número inteiro: %d\n", &x);
+	
+	if (x%2 == 0) {
+	
+		printf("par\n");	
+	}
+	
+	else {
+		
+		printf("impar\n");
+	}
+	
+	return 0;
+}
+	
