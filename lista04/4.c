15a8f2f7f6eab7580080e24131dca829bb0dacfe
richardiago <iagorichard@outlook.com>
2017-11-24 23:50:29 -0200

    potencia de 2

--- lista04/4.c
+++ lista04/4.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <time.h>
+#include <limits.h>
 
-int f1(int x)
+#define BIGNUM ULONG_MAX
+
+typedef unsigned long int ulint;
+
+ulint f1(ulint x)
 {
 	int y = 1;
 	
@@ -10,7 +16,7 @@ int f1(int x)
 	return 1;
 }
 
-int f2(int x)
+ulint f2(ulint x)
 {
 	int y = 1;
 	
@@ -22,13 +28,28 @@ int f2(int x)
 
 int main(void)
 {
-	int x, y;
-	scanf("%d", &x);
+	clock_t tempo_init, tempo_fim;
+	double tempo_gasto;
+	ulint soma = 0;
 	
-	y = f2(x);
+	tempo_init = clock();
+	for (int i=0; i<BIGNUM; i++) {
+		soma +=f1(i);
+	}
 	
-	if (y == 0) printf("sim\n");
-	else printf("nao\n");
+	tempo_fim = clock();
+	tempo_gasto = (double)(tempo_fim - tempo_init)/CLOCKS_PER_SEC;
+	printf("Tempo gasto na versão normal: %lf\n", tempo_gasto);
+	
+	tempo_init = clock();
+	for (int i=0; i<BIGNUM; i++) {
+		soma +=f2(i);
+	}
+	
+	tempo_fim = clock();
+	tempo_gasto = (double)(tempo_fim - tempo_init)/CLOCKS_PER_SEC;
+	printf("Tempo gasto na versão bitwise: %lf\n", tempo_gasto);
+	
 	
 	return 0;
 }
